da84577e25cce9bcbfb1448da2112a81f6316d21
LHAWLEY <hawleyleeanne@gmail.com>
2017-05-10 20:57:37 -0500

    Add cycle check function and tests

--- index.js
+++ index.js
@@ -24,26 +24,66 @@ module.exports = {
    * @param {object} packages - Object of library-dependency key-value pairs
   **/
   sort(packages = {}) {
-    const order = _.keys(_.pick(packages, _.isEmpty));
-    packages = _.omit(packages, _.isEmpty);
+    const noDeps = _.pick(packages, _.isEmpty);
+    const withDeps = _.omit(packages, _.isEmpty);
+    const ordered = [..._.keys(noDeps)];
 
-    while (_.any(packages)) {
-
-
-      for (const library in packages) {
-        if (order.indexOf(packages[library]) >= 0) {
-          order.push(library);
-          delete packages[library];
+    while (_.any(withDeps)) {
+      for (const library in withDeps) {
+        if (_.contains(ordered, withDeps[library])) {
+          ordered.push(library);
+          delete withDeps[library];
         }
       }
     }
 
-    return order;
+    return ordered;
   },
 
+  /**
+   * Parses packages and checks if there is a cycle
+   * if there is a cycle it throws an error
+   * if there is no cycle it returns a string of the sorted packages
+   * @param {array} libraries - The list of libraries.
+  **/
   install(packages = []) {
-    return this.sort(this.parse(packages)).join(', ');
+    const libraries = this.parse(packages);
+
+    if (this.isCycle(libraries)) {
+      throw("This is a cycle");
+    } else {
+      return this.sort(libraries).join(', ');
+    }
   },
 
+  /**
+   * Returns true or false after checking to see if an object of packages contains a cycle
+   * @param {object} packages - Object of library-dependency key-value pairs
+  **/
+  isCycle(packages = {}) {
+    const libraries = _.chain(packages).pairs().flatten().uniq().value();
 
+    return !_.all(libraries, library => {
+      let result = true;
+      const checked = [];
+
+      if (!packages[library] || _.contains(checked, library)) {
+        return true;
+      }
+
+      while (packages[library]) {
+        const dependency = packages[library];
+        checked.push(library);
+
+        if (_.contains(checked, dependency)) {
+          result = false;
+          break;
+        }
+
+        library = dependency;
+      }
+
+      return result;
+    });
+  }
 };
--- test/test.js
+++ test/test.js
@@ -26,12 +26,50 @@ describe('#sort', function () {
 });
 
 describe('#install', function () {
-  it('returns a comma delimited string of libraries in the order they should be installed', function () {
-    const input = ['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser: KittenService','Fraudstream: Leetmeme','Ice: '];
+  context('if there is no cycle', function () {
+    it('returns a comma delimited string of libraries in the order they should be installed', function () {
+      const input = ['KittenService: ', 'Leetmeme: Cyberportal', 'Cyberportal: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: '];
 
-    expect(index.install(input)).to.deep.eq(
-      'KittenService, Ice, Cyberportal, CamelCaser, Leetmeme, Fraudstream'
-    );
-  });
+      console.log('OUTPUT:', index.install(input));
+
+      expect(index.install(input)).to.deep.eq(
+        'KittenService, Ice, Cyberportal, CamelCaser, Leetmeme, Fraudstream'
+      );
+    });
+  })
+
+
+  context('if there is a cycle', function (){
+    it('should fail', function () {
+      const input = ['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser: KittenService','Fraudstream: ','Ice: Leetmeme'];
+
+      expect(function() {index.install(input)}).to.throw(
+        "This is a cycle"
+      )
+    })
+  })
 });
 
+describe('#isCycle', function () {
+  context('given a cyclical graph', function () {
+    it('is true', function () {
+      const cyclical = { A: 'B', B: 'C', C: 'A'};
+
+      expect(index.isCycle(cyclical)).to.eq(true);
+    });
+  });
+
+  context('given an acyclical graph', function () {
+    it('is false', function () {
+      const acyclical = { A: 'B', B: 'C', C: 'D', D: ''};
+
+      expect(index.isCycle(acyclical)).to.eq(false);
+    });
+
+    it('is false', function () {
+      const acyclical = index.parse(['KittenService: ', 'Leetmeme: Cyberportal', 'Cyberportal: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: ']);
+
+      expect(index.isCycle(acyclical)).to.eq(false);
+    });
+  });
+});
